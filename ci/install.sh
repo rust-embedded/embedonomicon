e9f1c2d0ac25ae586fbb7e09033ff78295216286
Jorge Aparicio <jorge@japaric.io>
2018-08-30 23:06:57 +0200

    install cargo-binutils

--- ci/install.sh
+++ ci/install.sh
@@ -11,6 +11,11 @@ main() {
 
     rustup target add thumbv7m-none-eabi
 
+    rustup component add llvm-tools-preview
+
+    curl -LSfs https://japaric.github.io/trust/install.sh | \
+        sh -s -- --git rust-embedded/cargo-binutils --tag v0.1.2
+
     pip install linkchecker --user
 }
 
