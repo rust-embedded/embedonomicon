334105f893f3695eabc722a157e8408ab7d94a45
Jorge Aparicio <jorge@japaric.io>
2018-09-24 02:16:01 +0200

    chmod +x

--- ci/install.sh
+++ ci/install.sh
@@ -18,6 +18,7 @@ main() {
 
     mkdir qemu
     curl -L https://github.com/japaric/qemu-bin/blob/master/14.04/qemu-system-arm-2.12.0 > qemu/qemu-system-arm
+    chmod +x qemu/qemu-system-arm
 
     rustup component add llvm-tools-preview
 
