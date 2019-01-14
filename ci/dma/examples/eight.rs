9f087674ff91d23451a5a521d07916940afae7be
Emil Fresk <emil.fresk@gmail.com>
2019-01-14 15:39:06 +0100

    Update ci/dma/examples/eight.rs
    Co-Authored-By: japaric <jorge@japaric.io>

--- ci/dma/examples/eight.rs
+++ ci/dma/examples/eight.rs
@@ -30,7 +30,7 @@ struct Inner<B> {
 impl<B> Transfer<B> {
     /// Blocks until the transfer is done and returns the buffer
     pub fn wait(mut self) -> (Pin<B>, Serial1) {
-        while self.is_done() {}
+        while !self.is_done() {}
 
         atomic::compiler_fence(Ordering::Acquire);
 
